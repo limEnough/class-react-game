e8d0eb0fe33c03c7961fe93fba69655465455836
limEnough <limEnough@gmail.com>
2024-06-10 18:37:03 +0900

    feat: update the state correctly based on the old state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,20 +1,33 @@
 import {useState} from 'react';
 
 export default function Player({name, symbol}) {
+  // #region State
   const [ isEditing, setIsEditing ] = useState(false);
   const [ userName, setUserName ] = useState(name); 
+  // #endregion
 
-  const handleChangedValue = (e) => {
-    setUserName(e.target.value)
+  // #region Computed
+  const playerName = isEditing ? <input type="text" value={userName} required onChange={(e) => handleChangeValue(e)}/> : <span className="player-name">{userName}</span>; 
+  // #endregion
+
+  // #region Events
+  const handleChangeValue = (e) => {
+    setUserName(() => e.target.value);
+  }
+
+  const handleClickButon = () => {
+    /**
+     * NOTI: 상태를 이전값에 기반하여 변경하는 경우 상태 업데이트 함수에 새로운 함수를 전달해야 한다.
+     * setIsEditing(!isEditing); 이렇게 사용할 수 없는 이유? 스케줄 상태 업데이트가 제대로 되지 않기 때문에!
+     */
+    setIsEditing((editing) => !editing);
   }
 
   const saveChangedValue = () => {
-    setUserName(userName);
-    setIsEditing(false);
+    setUserName((userName) => userName);
+    setIsEditing((editing) => !editing);
   }
-  
-  const playerName = isEditing ? <input type="text" value={userName} required onChange={(e) => handleChangedValue(e)}/> : <span className="player-name">{userName}</span>; 
-  const buttonCaption = isEditing ? 'Save' : 'Edit';
+  // #endregion
 
   return (
     <li>
@@ -22,7 +35,7 @@ export default function Player({name, symbol}) {
       {
         isEditing
         ? <button type="button" onClick={() => saveChangedValue()}>Save</button> 
-        : <button type="button" onClick={() => setIsEditing(!isEditing)}>Edit</button> 
+        : <button type="button" onClick={() => handleClickButon()}>Edit</button> 
       }
     </li>
   )
